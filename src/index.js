73e89bdbb1dc5571d1fe9d0e001ec6709b6d549f
NikolaevNikitaD <375298057622@yandex.ru>
2022-08-12 22:37:14 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    if (!number) return "zero";
+    let arr1 = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    let arr2 = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    let arr3 = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    let str = "";
+    if (number % 100 > 9 && number % 100 < 20) str = arr2[number % 10];
+    else
+        str = (
+            arr3[Math.trunc(number / 10) % 10] +
+            " " +
+            arr1[number % 10]
+        ).trim();
+    number = Math.trunc(number / 100);
+    if (number) str = (arr1[number] + " hundred " + str).trim();
+    return str;
+};
